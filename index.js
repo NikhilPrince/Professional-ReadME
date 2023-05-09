04961791c678db5c95f991f7f9228ab51705642b
NikhilPrince <117137916+NikhilPrince@users.noreply.github.com>
2023-05-08 18:41:27 -0700

    added questions

--- index.js
+++ index.js
@@ -1,13 +1,148 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please name your project? (Required)',
+        validate: projectInput => {
+            if (projectInput) {
+                return true;
+            } else {
+                console.log('Please name your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'githubname',
+        message: 'Please provide your GitHub username? (Required)',
+        validate: usernameInput => {
+            if (usernameInput) {
+                return true;
+            } else {
+                console.log('Please provide your GitHub username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'repo name',
+        message: 'Please provide the name of your repo? (Required)',
+        validate: repoInput => {
+            if (repoInput) {
+                return true;
+            } else {
+                console.log('Please provide the name of your repo!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Please provide a small description of your project? (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please provide a small description of your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Reason',
+        message: 'What is your reason for making this project? (Required)',
+        validate: reasonInput => {
+            if (reasonInput) {
+                return true;
+            } else {
+                console.log('What is your reason for making this project!');
+                return false;
+            }
+        }
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
+    {
+        type: 'input',
+        name: 'steps',
+        message: 'Please provide the necessary steps to install your project. (Required)',
+        validate: stepsInput => {
+            if (stepsInput) {
+                return true;
+            } else {
+                console.log('Please provide the necessary steps to install your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license will you use for your project?',
+        choices: ['agpl', 'apache', 'mit', 'no license']
+    },
+ 
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'Instructions on testing this application. (Required)',
+        validate: instructionsInput => {
+            if (instructionsInput) {
+                return true;
+            } else {
+                console.log('Instructions on testing this application!');
+                return false;
+            }
+        }
+    }
+];
+
+
+const storeFile = contents => {
+    return new Promise((resolve, reject) => {
+        fs.storeFile('./dist/generated-README.md', contents, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+
+const init = () => {
+
+    return inquirer.prompt(questions)
+    .then(contentOfREADME => {
+        return contentOfREADME;
+    })
+}
 
 // Function call to initialize app
-init();
+init()
+.then(contentOfREADME => {
+    console.log(contentOfREADME);
+    return generateMarkdown(contentOfREADME);
+})
+.then(pageMD => {
+    return storeFile(pageMD);
+})
+.then(storeRes => {
+    console.log(storeRes.message);
+})
+.catch(err => {
+    console.log(err);
+})
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,92 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) { 
+  if (license) {
+    return `
+  ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function createLicenseLinkk(license) {
+  if (license) {
+  return `
+  [${license}](https://choosealicense.com/licenses/${license})
+    `;
+  } else {
+    return '';
+  }
+}
 
+
+function createLicenseSec(license) {
+  if (license) {
+  return `
+  ## [License](#Table-Of-Contents)
+
+  ${createLicenseLinkk(license)}
+    `;
+  } else {
+    return ' ';
+  }
+ }
+
+function createLicenceContent(license) {
+  if (license) {
+  return `
+  * [License](#license)
+    `;
+  } else {
+    return ' ';
+  }
+ }
+
+
+function generateMarkdown(info) {
+  return `
+  # ${info.title}
+  
+  ${renderLicenseBadge(info.license)}
+
+  ## Table-Of-Contents
+
+  * [Description](#description)
+  * [StepsToInstall](#StepsToInstall)
+  * [Usage](#usage)
+  ${createLicenceContent(info.license)}
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## [Description](#Table-Of-Contents)
+
+  ${info.repo}
+
+  ${info.description}
+
+  ${info.reason}
+
+  ## [StepsToInstall](#Table-Of-Contents)
+
+  ${info.StepsToInstall}
+
+  ## [Usage](#Table-Of-Contents)
+ 
+  ${createLicenseSec(info.license)}
+  
+  ## [Tests](#Table-Of-Contents)
+
+  ${info.steps}
+
+  ## [Questions](#Table-Of-Contents)
+
+  Please contact me using the following links:
+
+  [GitHub](https://github.com/${info.githubname})
+
+  [Email: ${info.email}](mailto:${info.email})
 `;
 }
 
